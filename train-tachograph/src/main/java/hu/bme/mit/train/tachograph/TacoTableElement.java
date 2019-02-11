0f2e030381c2a34f62c4d3458ce95e276f11a1e3
meres <izso.andras.zsolt@gmail.com>
2019-02-11 08:50:24 -0800

    Added Guava table

---
+++ train-tachograph/src/main/java/hu/bme/mit/train/tachograph/TacoTableElement.java
@@ -0,0 +1,21 @@
+package hu.bme.mit.train.tachograph;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Date;
+
+import org.junit.jupiter.api.Test;
+
+import junit.framework.Assert;
+
+class TacoTableElement {
+
+	@Test
+	void tacotastetest() {
+		Tachograph table = new Tachograph();
+		table.addTableElement(new Date(), 5, 7);
+		
+		Assert.assertEquals(false, table.isEmpty());
+	}
+
+}
