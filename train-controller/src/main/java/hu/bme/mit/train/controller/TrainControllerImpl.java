eeb79c87c366650b45762ce341647d0c27209a73
meres <izso.andras.zsolt@gmail.com>
2019-02-11 06:39:28 -0800

    Added build error

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -8,6 +8,7 @@ public class TrainControllerImpl implements TrainController {
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
 
+	error = 5;
 	@Override
 	public void followSpeed() {
 		if (referenceSpeed < 0) {
