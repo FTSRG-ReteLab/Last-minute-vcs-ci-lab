12edec19ac8be2641c159e5352e1e21dd4ef55b6
Izso Andras <izso.andras.zsolt@gmail.com>
2019-04-01 05:58:28 -0700

    Joystick position poll

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -2,11 +2,26 @@ package hu.bme.mit.train.controller;
 
 import hu.bme.mit.train.interfaces.TrainController;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class TrainControllerImpl implements TrainController {
 
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
+	Timer updateTimer = new Timer();
+
+	public TrainControllerImpl(){
+		TimerTask updateTask = new TimerTask(){
+			public void run(){
+				followSpeed();
+			}
+		};
+
+		updateTimer.schedule(updateTask, 0, 500);
+	}
+
 
 	@Override
 	public void followSpeed() {
@@ -43,7 +58,8 @@ public class TrainControllerImpl implements TrainController {
 
 	@Override
 	public void setJoystickPosition(int joystickPosition) {
-		this.step = joystickPosition;		
+		this.step = joystickPosition;
+
 	}
 
 }
